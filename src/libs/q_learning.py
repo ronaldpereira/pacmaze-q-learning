61d303cef353fa9eca534e793377e9ac7a28a9b4
Ronald Pereira <ronald.drp11@gmail.com>
2019-06-13 12:44:12 -0300

    added maze object to qlearning lib

--- src/libs/q_learning.py
+++ src/libs/q_learning.py
@@ -1,6 +1,7 @@
 class QLearning:
-    def __init__(self, alpha, epsilon, n):
+    def __init__(self, alpha, epsilon, n, maze):
         self.gamma = 0.9
         self.alpha = alpha
         self.epsilon = epsilon
         self.n = n
+        self.maze = maze
--- src/pacmaze.py
+++ src/pacmaze.py
@@ -4,4 +4,4 @@ import libs.q_learning as QL
 
 maze = MZ.Maze(argv[1])
 
-qlearning = QL.QLearning(argv[2], argv[3], argv[4])
+qlearning = QL.QLearning(argv[2], argv[3], argv[4], maze)
