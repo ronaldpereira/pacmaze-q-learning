5691a5f0df7ecb09e54173aca42bb46e82c3ce64
Ronald <ronald.drp11@gmail.com>
2019-06-15 22:54:34 -0300

    qlearning init

--- src/libs/q_learning.py
+++ src/libs/q_learning.py
@@ -1,7 +1,15 @@
+import numpy as np
+
+
 class QLearning:
-    def __init__(self, alpha, epsilon, n, maze):
+    def __init__(self, alpha, epsilon, n, maze, start=(1, 1)):
         self.gamma = 0.9
         self.alpha = alpha
         self.epsilon = epsilon
         self.n = n
         self.maze = maze
+        self.actions = {"U": 0, "D": 1, "L": 2, "R": 3}
+        self.q = np.zeros(
+            (len(self.actions), self.maze.lines, self.maze.columns), dtype=np.float64
+        )
+        self.position = start
--- src/pacmaze.py
+++ src/pacmaze.py
@@ -5,4 +5,8 @@ import libs.q_learning as QL
 
 maze = MZ.Maze(argv[1])
 
-qlearning = QL.QLearning(argv[2], argv[3], argv[4], maze)
+qlearning = QL.QLearning(argv[2], argv[3], argv[4], maze, (3, 1))
+
+print(maze.maze)
+print(maze.rewards)
+print(qlearning.q)
